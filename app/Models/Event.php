16f9b65bcdacd71186127c0801f3ee552f5ce677
Dries-Michiels <driesmichiels73@gmail.com>
2026-01-07 23:26:21 +0100

    feat: add events model + migration

---
+++ app/Models/Event.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'starts_at',
+        'ends_at',
+        'location',
+        'ticket_url',
+        'image_path',
+        'description',
+    ];
+
+    protected $casts = [
+        'starts_at' => 'datetime',
+        'ends_at' => 'datetime',
+    ];
+
+    public function isUpcoming(): bool
+    {
+        return $this->starts_at->isFuture();
+    }
+}
